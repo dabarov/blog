89c4ffe1a2fe02c8063fac544412d717ff4770fd
dabarov <aldiyar.dabarov@nu.edu.kz>
2022-02-27 11:29:02 +0600

    feat: add sitemap generator plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,6 +129,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`,
   ],
   pathPrefix: `/blog`,
 }
